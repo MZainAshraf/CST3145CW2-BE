6b53a1296fbb8bd02f59c4a162fb2bdb2373d20e
MZainAshraf <99812309+MZainAshraf@users.noreply.github.com>
2022-02-17 01:49:38 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+
+app.use ((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+})
+
+
+const MongoClient = require('mongodb').MongoClient;
+let db;
+MongoClient.connect('mongodb+srv://zain:Pakistan2@cluster0.e2ql2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', (err, client) => {
+    db = client.db('programs')
+})
+
+
+// dispaly a message for root path to show that API is working
+app.get('/', (req, res, next) => {
+    res.send('Select a collection, e.g., /collection/messages')
+})
+
+// get the collection name
+app.param('collectionName', (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName)
+    // console.log('collection name:', req.collection)
+    return next()
+})
+
+// retrieve all the objects from an collection
+app.get('/collection/:collectionName', (req, res, next) => {
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next(e)
+        res.send(results)
+    })
+})
+
+//adding post
+app.post('/collection/:collectionName', (req, res, next) => {
+req.collection.insert(req.body, (e, results) => {
+if (e) return next(e)
+res.send(results.ops)
+})
+})
+
+// return with object id 
+
+const ObjectID = require('mongodb').ObjectID;
+app.get('/collection/:collectionName/:id'
+, (req, res, next) => {
+req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+if (e) return next(e)
+res.send(result)
+})
+})
+
+
+//update an object 
+
+app.put('/collection/:collectionName/:id', (req, res, next) => {
+req.collection.update(
+{_id: new ObjectID(req.params.id)},
+{$set: req.body},
+{safe: true, multi: false},
+(e, result) => {
+if (e) return next(e)
+res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'})
+})
+})
+
+
+
+
+
+app.delete('/collection/:collectionName/:id', (req, res, next) => {
+req.collection.deleteOne(
+{ _id: ObjectID(req.params.id) },(e, result) => {
+if (e) return next(e)
+res.send((result.result.n === 1) ?
+{msg: 'success'} : {msg: 'error'})
+})
+})
+
+
+const port = process.env.PORT || 3000
+app.listen(port)
